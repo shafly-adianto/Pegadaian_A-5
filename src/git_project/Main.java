5ba7c3ba9858fe192e8a84f8dab4c5b715b66b3b
septyani <septyani@users.noreply.github.com>
2019-11-05 21:19:39 +0700

    Update Main.java

--- src/git_project/Main.java
+++ src/git_project/Main.java
@@ -1,7 +1,6 @@
 package git_project;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
 	static Main main = new Main();
@@ -9,49 +8,54 @@ public class Main {
 	Gadai gd = new Gadai();
 	ArrayList<Gadai> data_gadai = new ArrayList<>();
 
+	public String centerString (int width, String s) {
+	    return String.format("%-" + width  + "s", 
+	    		String.format("%" + (s.length() + (width - s.length()) / 2) + "s", s));
+	}
+	
 	public void inputGadai() {
 		input.nextLine();
 		String nama, productCategory, description;
 		int price, id;
 		boolean valid = false;
-
+	
 		do {
 			System.out.print("Nama :");
 			nama = input.nextLine();
-
+			
 			if (nama.length() >= 3 && nama.length() <= 15) {
 				gd.setNama(nama);
 				valid = true;
 			}
 		} while (!valid);
-
+		
 		do {
 			valid = false;
 			System.out.print("Product Category :");
 			productCategory = input.nextLine();
-
+			
 			if (productCategory.equalsIgnoreCase("laptop") || productCategory.equalsIgnoreCase("motor") || productCategory.equalsIgnoreCase("emas")) {
 				gd.setProductCategory(productCategory);
 				valid = true;
 			}
 		} while (!valid);
-
+		
 		do {
 			valid = false;
 			System.out.print("Description :");
 			description = input.nextLine();
-
+			
 			if (description.indexOf(" ") >= 1) {
 				gd.setDescription(description);
 				valid = true;
 			}
 		} while (!valid);
-
+		
 		do {
 			valid = false;
 			System.out.print("Price :");
 			price = input.nextInt();
-
+			
 			if (price%10000 == 0) {
 				gd.setPrice(price);
 				valid = true;
@@ -60,87 +64,124 @@ public class Main {
 		id = data_gadai.size() +1;
 		data_gadai.add(new Gadai(nama, productCategory, description, "Gadai", id, price, price));
 	}
-
+	
 	public void showTebus() {
-			int id, biaya;
-			int checkId =0 , checkBiaya =0;
-			boolean valid;
-
-			if (data_gadai.isEmpty()==true) {
-				System.out.println("Data gadai kosong, silahkan input gadai terlebih dulu.");
+		int id, biaya;
+		int checkId =0 , checkBiaya =0;
+		boolean valid;
+		
+		String header = "|"+centerString(8, "Id")+"|"+centerString(12, "Produk")+"|"+centerString(12, "Price")+"|"+centerString(12, "Status")+"|"+centerString(12, "Utang")+"|";
+		String caption = "|"+centerString(header.length()-2, "LIST GADAI")+"|";
+		String line = "";
+		String stripe = "";
+		
+		for (int i = 0; i < caption.length(); i++) {
+			line+="=";
+			if (i>1) {
+				stripe+="-";
 			}
-			else {
+		}	
+		
+		if (data_gadai.isEmpty()==true) {
+			System.out.println("Data gadai kosong, silahkan input gadai terlebih dulu.");
+		}
+		else {
+			System.out.println("\n"+line);
+			System.out.println(caption+"\n|"+stripe+"|\n"+header+"\n|"+stripe+"|");
+			for (Gadai gadai : data_gadai) {
+				System.out.println(gadai.getId()+" "+gadai.getProductCategory()+" "+ gadai.getPrice()+" "+gadai.getStatus()+" "+gadai.getHutang());
+			}
+			System.out.println(line+"\n");
+			
+			do {
+				valid = false;
+				System.out.println("Masukkan id barang yang ingin di tebus :");
+				id = input.nextInt();
+				System.out.println("\nMasukkan biaya yang ingin di tebus :");
+				biaya = input.nextInt();
+				
 				for (Gadai gadai : data_gadai) {
-					System.out.println(gadai.getId()+" "+gadai.getProductCategory()+" "+ gadai.getPrice()+" "+gadai.getStatus()+" "+gadai.getHutang());
-				}
-
-				do {
-					valid = false;
-					System.out.println("\nMasukkan id barang yang ingin di tebus :");
-					id = input.nextInt();
-					System.out.println("\nMasukkan biaya yang ingin di tebus :");
-					biaya = input.nextInt();
-
-					for (Gadai gadai : data_gadai) {
-						if (gadai.getId()==id) {
-							checkId+=1;
-						}
-						if (biaya <= gadai.getHutang()) {
-							checkBiaya+=1;
-						}
+					if (gadai.getId()==id) {
+						checkId+=1;
 					}
-
-					if (checkId > 0 && checkBiaya > 0) {
-						for (Gadai gadai : data_gadai) {
-							if (gadai.getId() == id) {
-								if (biaya <= gadai.getHutang()) {
-									gadai.setHutang(gadai.getHutang() - biaya);
-									if (gadai.getHutang()==0) {
-										gadai.setStatus("Lunas");
-									}
-									valid = true;
+					if (biaya <= gadai.getHutang()) {
+						checkBiaya+=1;
+					}
+				}
+				
+				if (checkId > 0 && checkBiaya > 0) {
+					for (Gadai gadai : data_gadai) {
+						if (gadai.getId() == id) {
+							if (biaya <= gadai.getHutang()) {
+								gadai.setHutang(gadai.getHutang() - biaya);
+								if (gadai.getHutang()==0) {
+									gadai.setStatus("Lunas");
 								}
+								valid = true;
 							}
 						}
 					}
-					else if (checkId == 0 && checkBiaya > 0) {
-						System.out.println("\nId tidak ditemukkan");
-						valid = false;
-					}
-					else if (checkId > 0 && checkBiaya == 0) {
-						System.out.println("\nBiaya yang dimasukkan melebihi hutang");
-						valid = false;
-					}
-					else {
-						System.out.println("\nId tidak ditemukkan");
-						valid = false;
-					}
-
-				} while (!valid);
-				
-
-				for (Gadai gadai : data_gadai) {
-					System.out.println(gadai.getId()+" "+gadai.getProductCategory()+" "+ gadai.getPrice()+" "+gadai.getStatus()+" "+gadai.getHutang());
 				}
+				else if (checkId == 0 && checkBiaya > 0) {
+					System.out.println("\nId tidak ditemukkan");
+					valid = false;
+				}
+				else if (checkId > 0 && checkBiaya == 0) {
+					System.out.println("\nBiaya yang dimasukkan melebihi hutang");
+					valid = false;
+				}
+				else {
+					System.out.println("\nId tidak ditemukkan");
+					valid = false;
+				}
+				
+			} while (!valid);
+			
+			System.out.println("\n"+line);
+			System.out.println(caption+"\n|"+stripe+"|\n"+header+"\n|"+stripe+"|");
+			System.out.println(line+"\n");
+			for (Gadai gadai : data_gadai) {
+				System.out.println(gadai.getId()+" "+gadai.getProductCategory()+" "+ gadai.getPrice()+" "+gadai.getStatus()+" "+gadai.getHutang());
 			}
+			System.out.println(line+"\n");
 		}
+	}
+
 	public void showInventori() {
 		int hitungBarang = 0;
+		
+		String header = "|"+centerString(8, "Id")+"|"+centerString(15, "Nama")+"|"+centerString(12, "Product")+"|"+centerString(18, "Description")+"|"+centerString(12, "Price")+"|";
+		String caption = "|"+centerString(header.length()-2, "LIST GADAI")+"|";
+		String line = "";
+		String stripe = "";
+		
+		for (int i = 0; i < caption.length(); i++) {
+			line+="=";
+			if (i>1) {
+				stripe+="-";
+			}
+		}
+		
+		System.out.println("\n"+line);
+		System.out.println(caption+"\n|"+stripe+"|\n"+header+"\n|"+stripe+"|");
+		System.out.println(line+"\n");
 		for (Gadai gadai : data_gadai) {
 			if (gadai.getStatus().equalsIgnoreCase("gadai")) {
 				hitungBarang+=1;
 				System.out.println(gadai.getNama()+" "+gadai.getProductCategory()+" "+gadai.getDescription()+" "+gadai.getPrice());
 			}
-		}if (hitungBarang==0) {
+		}
+		System.out.println(line+"\n");
+		
+		if (hitungBarang==0) {
 			System.out.println("Tidak ada barang gadai di inventori");
 		}
-		
 	}
-
+	
 	public static void main(String[] args) {
 		input = new Scanner(System.in);
 		boolean valid;
-
+		
 		while(true) {
 			do {
 				valid = true;
@@ -162,19 +203,17 @@ public class Main {
 			}while(!valid);
 		}
 	}
-
+	
 	public void inputan(int nomor) {
 		switch(nomor) {
 		case 1:
-			System.out.println("Gadai");
 			main.inputGadai();
 			break;
 		case 2:
-			System.out.println("Tebus");
 			main.showTebus();
 			break;
 		case 3:
-			System.out.println("Tampilkan Inventory");
+			main.showInventori();
 			break;
 		case 4:
 			System.out.print("Exit");
@@ -184,14 +223,6 @@ public class Main {
 			System.out.print("Nomor program tidak ada");
 			break;
 		}
-
-	}
-	public static void TampilGadai(List<Gadai> data_gadai){
-		for (Gadai g:data_gadai){
-			if (g.getStatus().equales("lunas")){
-				 System.out.format(Format, g.getId(), g.getNama(),g.getProductCategory(),g.getDescription(),g.getPrice());
-				System.out.format(Format, );
-			}
-		}
+		
 	}
 }
