a3a177e306990291f5efe023ecc031f364d67fea
Caesar Richard Pitoy <caesar.pitoy@pegadaian.co.id>
2019-11-05 20:45:27 +0700

    edit main

--- src/git_project/Main.java
+++ src/git_project/Main.java
@@ -1,85 +1,33 @@
 package git_project;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 	static Main main = new Main();
 	static Scanner input = new Scanner(System.in);
 	Gadai gd = new Gadai();
-	
-	public void inputGadai() {
-		String nama, productCategory, description;
-		int price;
-		boolean valid = false;
-	
-		do {
-			input.nextLine();
-			System.out.print("Nama :");
-			nama = input.nextLine();
-			
-			if (nama.length() >= 3 && nama.length() <= 15) {
-				gd.setNama(nama);
-				valid = true;
-			}
-		} while (!valid);
-		
-		do {
-			valid = false;
-			System.out.print("Product Category :");
-			productCategory = input.nextLine();
-			
-			if (productCategory.equalsIgnoreCase("laptop") || productCategory.equalsIgnoreCase("motor") || productCategory.equalsIgnoreCase("emas")) {
-				gd.setProductCategory(productCategory);
-				valid = true;
-			}
-		} while (!valid);
-		
-		do {
-			valid = false;
-			System.out.print("Description :");
-			description = input.nextLine();
-			
-			if (description.indexOf(" ") >= 1) {
-				gd.setDescription(description);
-				valid = true;
-			}
-		} while (!valid);
-		
-		do {
-			valid = false;
-			System.out.print("Price :");
-			price = input.nextInt();
-			
-			if (price%10000 == 0) {
-				gd.setPrice(price);
-				valid = true;
-			}
-		} while (!valid);
-		
-	}
-	
-
+	ArrayList<Gadai> data_gadai = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		input = new Scanner(System.in);
 		boolean valid;
 		
-		System.out.println("Pegadaian System: ");
-		System.out.println("1. Gadai");
-		System.out.println("2. Tebus");
-		System.out.println("3. Tampilkan Inventory");
-		System.out.println("4. Exit");
-		
 		while(true) {
 			do {
 				valid = true;
 				try {
+					System.out.println("Pegadaian System: ");
+					System.out.println("1. Gadai");
+					System.out.println("2. Tebus");
+					System.out.println("3. Tampilkan Inventory");
+					System.out.println("4. Exit");
 					System.out.print("\n\nMasukan nomor program: ");
 					int nomor = input.nextInt();
 					main.inputan(nomor);
 				}
 				catch(Exception e){
 					valid = false;
-					System.out.print("Format harus angka");
+					System.out.print("Sesuaikan Format Inputtan!!!");
 					input = new Scanner(System.in);
 				}
 			}while(!valid);
@@ -89,14 +37,13 @@ public class Main {
 	public void inputan(int nomor) {
 		switch(nomor) {
 		case 1:
-			main.inputGadai();
-			System.out.println(gd.getPrice());
+			
 			break;
 		case 2:
-			System.out.print("Tebus");
+			
 			break;
 		case 3:
-			System.out.print("Tampilkan Inventory");
+			
 			break;
 		case 4:
 			System.out.print("Exit");
